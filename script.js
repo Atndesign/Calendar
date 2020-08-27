f920745742064fa1ee5749da4166c2f1b036f545
Kevin <Atnnetworkk@gmail.com>
2020-08-27 20:06:54 +0200

    Add getDaysInMonth function

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const domContainer = document.querySelector("#container");
-ReactDOM.render(<App></App>, domContainer);
+/*
+
+- Get the number of days in a month V
+- Generate the number of days
+- Be able to switch from month to month
+- Convert text to Date
+
+*/
+
+function getDaysInMonth(date) {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+}
+
+function generateDays() {
+  let days = getDaysInMonth(new Date(2020, 9));
+  for (day = 1; day <= days; day++) {
+    console.log(day);
+  }
+}
+
+generateDays();
