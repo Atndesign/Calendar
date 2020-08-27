02a2cdf847af689f85506cf28d0cd6e47ab7bec8
Kevin <Atnnetworkk@gmail.com>
2020-08-27 20:21:58 +0200

    Add query selector for date month and full date

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 /*
 
 - Get the number of days in a month V
-- Generate the number of days
+- Generate the number of days V
+- Check the first day of the week
 - Be able to switch from month to month
 - Convert text to Date
 
 */
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "November",
+  "December",
+];
+
+let date = new Date();
+
+document.querySelector(".date__month").innerHTML = months[date.getMonth()];
+document.querySelector(".date__full-date").innerHTML = date.toDateString();
 
 function getDaysInMonth(date) {
   return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 }
 
 function generateDays() {
+  let calendar = document.getElementById("calendar");
   let days = getDaysInMonth(new Date(2020, 9));
   for (day = 1; day <= days; day++) {
-    console.log(day);
+    calendar.innerHTML += `<p class="numbers__number">${day}</p>`;
   }
 }
 
