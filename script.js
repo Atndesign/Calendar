e568ba1965e50bda5554c0c20e010482585c296f
Kevin <Atnnetworkk@gmail.com>
2020-08-27 22:18:33 +0200

    Add render method and generateDay functions

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 - Convert text to Date
 
 */
-
+const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const months = [
   "January",
   "February",
@@ -18,20 +18,47 @@ const months = [
   "July",
   "August",
   "September",
+  "October",
   "November",
   "December",
 ];
 
 let date = new Date();
+let calendar = document.getElementById("calendar");
 
 date.setDate(1);
 
-document.querySelector(".date__month").innerHTML = months[date.getMonth()];
-document.querySelector(".date__full-date").innerHTML = date.toDateString();
+function displayDate() {
+  document.querySelector(".date__month").innerHTML = months[date.getMonth()];
+  document.querySelector(".date__full-date").innerHTML = date.toDateString();
+}
+
+const firstDayIndex = date.getDay();
+let prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+const lastDayIndex = new Date(
+  date.getFullYear(),
+  date.getMonth() + 1,
+  0
+).getDay();
+
+const nextDays = 7 - lastDayIndex;
+
+function generatePastDays() {
+  for (lastDay = firstDayIndex; lastDay > 0; lastDay--) {
+    calendar.innerHTML += `<p class="numbers__number --grey">${
+      prevLastDay - lastDay + 1
+    }</p>`;
+  }
+}
+
+function generateNextDays() {
+  for (nextDay = 1; nextDay <= nextDays; nextDay++) {
+    calendar.innerHTML += `<p class="numbers__number --grey">${nextDay}</p>`;
+  }
+}
 
 function generateDays() {
-  let calendar = document.getElementById("calendar");
-  let days = getDaysInMonth(new Date(2020, 9));
+  let days = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
   for (day = 1; day < days; day++) {
     if (
       day === new Date().getDate() &&
@@ -44,6 +71,30 @@ function generateDays() {
   }
 }
 
-generatePastDays();
-generateDays();
-generateNextDays();
+function generateDayName() {
+  for (i = 1; i <= 7; i++) {
+    calendar.innerHTML += `<p class="days__text">${dayNames[i - 1]}</p>`;
+  }
+}
+
+function renderCalendar() {
+  calendar.innerHTML = "";
+  displayDate();
+  generateDayName();
+  generatePastDays();
+  generateDays();
+  generateNextDays();
+}
+
+document.querySelector("#lastMonth").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector("#nexttMonth").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+date.setMonth(date.getMonth());
+renderCalendar();
